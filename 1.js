05d0c2568cc03d58756ce1587a6e53d3b0734cd8
GG <gg@GGs-MacBook-Air.wind3.hub>
2020-08-05 02:11:09 +0200

    try

---
+++ 1.js
@@ -0,0 +1,36 @@
+let counter = document.querySelector(".counter");
+const addcount = document.querySelector("#addCountBtn");
+const lowerCount = document.querySelector("#LowerCountBtn");
+
+let count = 0;
+
+addcount.addEventListener("click", incerementCounter);
+lowerCount.addEventListener("click", decerementCounter);
+
+function incerementCounter() {
+  count++;
+  counter.innerHTML = count;
+  if (counter.innerHTML > "0") {
+    counter.style.color = "#4caf50";
+  } else if (counter.innerHTML === "0") {
+    counter.style.color = "white";
+  }
+  counter.animate([{ opacity: "0.2" }, { opacity: "1.0" }], {
+    duration: 1000,
+    fill: "forwards",
+  });
+}
+
+function decerementCounter() {
+  count--;
+  counter.innerHTML = count;
+  if (counter.innerHTML < "0") {
+    counter.style.color = "red";
+  } else if (counter.innerHTML === "0") {
+    counter.style.color = "white";
+  }
+  counter.animate([{ opacity: "0.2" }, { opacity: "1.0" }], {
+    duration: 1000,
+    fill: "forwards",
+  });
+}
